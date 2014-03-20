1436a60a70b8c07f3c55a93239e8a055dab2d479
Tomas Charad <tomas.charad@gmail.com>
2014-03-20 11:54:42 -0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BerrincheSite::Application.config.secret_key_base = 'c4584e6a4c90679d227721b7528d92b682d4dae77f979a345b4c0ed19d846b8eccd70ddaa05fcede139e6c9947fbf66ff45b255ee054219961f38372cda8e944'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BerrincheSite::Application.config.session_store :cookie_store, key: '_berrinche-site_session'
